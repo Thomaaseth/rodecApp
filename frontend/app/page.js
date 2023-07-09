23d220131bbd2ed14b30a5092a975baee70710fc
Thomaaseth <thomas.demathieu@gmail.com>
2023-07-09 02:45:35 +0200

    add frontend files

---
+++ frontend/app/page.js
@@ -0,0 +1,12 @@
+"use client"
+import Image from 'next/image'
+import styles from './page.module.css'
+import Layout from '@/components/Layout/Layout'
+import { Children } from 'react'
+
+export default function Home() {
+  return (
+    <Layout>
+    </Layout>
+  )
+}
