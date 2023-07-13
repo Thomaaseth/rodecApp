7820197a957ccab14fc47366d7392b7b1cf30b4c
Thomaaseth <thomas.demathieu@gmail.com>
2023-07-13 04:40:41 +0200

    review contract/tests remix OK

--- backend/contracts/Donate.sol
+++ backend/contracts/Donate.sol
@@ -7,42 +7,49 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-/// @title Donation Contract
-/// @notice A contract for making donations and minting associated NFTs
-/// @dev Utilizes OpenZeppelin for secure, standardized functionality
+/**
+ * @title Donate contract
+ * @dev Implements donations in exchange for ERC721 tokens.
+ * @author Thomas for Alyra
+ */
 contract Donate is ERC721, ERC721Enumerable, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
+
+    // Counter for donation IDs
     Counters.Counter private _donationIds;
 
-    /// @notice The details of a specific donation
+    // Structure for donation details
     struct Donation {
         uint256 amount;
         uint256 timestamp;
     }
 
-    /// @notice Mapping from a donation ID to the specific details of that donation
+    // Mapping from donationId to Donation struct
     mapping(uint256 => Donation) public donations;
 
+    /**
+     * @dev Event emitted when a new donation is made
+     */
     event NewDonation(
-        address sender,
-        uint256 newDonationId,
+        address indexed sender,
+        uint256 indexed donationId,
         uint256 amount,
         uint256 timestamp
     );
 
+    // Minimum donation amount
     uint256 private _price = 50000000000000;
-    string public baseURI;
 
-    using Strings for uint;
+    /**
+     * @dev Sets the name and symbol of the token
+     */
+    constructor() ERC721("RoDec", "RDC") {}
 
-    constructor(string memory _baseURI) ERC721("RoDec", "RDC") {
-        baseURI = _baseURI;
-    }
-
-    /// @notice Make a donation and mint an associated NFT
-    /// @dev Donor must send enough Ether to cover the price of the donation
+    /**
+     * @dev Enables external accounts to make a donation and receive an ERC721 token
+     */
     function donate() external payable {
-        require(msg.value >= _price, "Amount needs to be superior to 0.00005");
+        require(msg.value >= _price, "Amount too low");
 
         uint256 newDonationId = _donationIds.current();
         _safeMint(msg.sender, newDonationId);
@@ -51,60 +58,45 @@ contract Donate is ERC721, ERC721Enumerable, Ownable, ReentrancyGuard {
             timestamp: block.timestamp
         });
         _donationIds.increment();
+
         emit NewDonation(msg.sender, newDonationId, msg.value, block.timestamp);
     }
 
-    /// @notice Set the base URI for all token IDs
-    function setBaseURI(string memory _baseURI) external onlyOwner {
-        baseURI = _baseURI;
-    }
-
-    function _getBaseURI()
-        internal
-        view
-        override(ERC721)
-        returns (string memory)
-    {
-        return baseURI;
-    }
-
-    /// @notice Get the URI for a specific token ID
-    function tokenURI(
-        uint _tokenId
-    ) public view virtual override(ERC721A) returns (string memory) {
-        require(_exists(_tokenId), "URI query for nonexistent token");
-
-        return string(abi.encodePacked(baseURI, _tokenId.toString(), ".json"));
-    }
-
-    /// @notice Set the price for making a donation
-    function setPrice(uint _newPrice) external onlyOwner {
-        _price = _newPrice;
-    }
-
-    /// @notice Get the current price for making a donation
+    /**
+     * @dev Returns the current donation price
+     */
     function getPrice() public view returns (uint256) {
         return _price;
     }
 
-    /// @notice Get the total number of donations made so far
+    /**
+     * @dev Allows the owner to set a new donation price
+     */
+    function setPrice(uint _newPrice) external onlyOwner {
+        _price = _newPrice;
+    }
+
+    /**
+     * @dev Returns the total number of donations made
+     */
     function nbDonationsTotal() external view returns (uint256) {
         return _donationIds.current();
     }
 
-    /// @notice Get the details for a specific donation
+    /**
+     * @dev Returns the details of a specific donation
+     */
     function getDonationDetails(
         uint256 donationId
     ) public view returns (uint256, uint256) {
-        require(
-            donations[donationId].timestamp != 0,
-            "Donation does not exist"
-        );
+        require(_exists(donationId), "Donation does not exist");
         Donation memory donation = donations[donationId];
         return (donation.amount, donation.timestamp);
     }
 
-    /// @notice Get the IDs of all donations made by a specific address
+    /**
+     * @dev Returns a list of donation IDs for a given address
+     */
     function walletOfOwner(
         address _owner
     ) public view returns (uint256[] memory) {
@@ -116,20 +108,30 @@ contract Donate is ERC721, ERC721Enumerable, Ownable, ReentrancyGuard {
         return donationsId;
     }
 
-    /// @notice Withdraw all Ether from the contract
-    function withdraw() external onlyOwner nonReentrant {
+    /**
+     * @dev Allows the owner to withdraw all funds from the contract
+     */
+    function withdraw() external payable onlyOwner {
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
         require(success, "Withdrawal failed");
     }
 
+    /**
+     * @dev Overrides _beforeTokenTransfer function to restrict transfers of tokens
+     */
     function _beforeTokenTransfer(
         address from,
         address to,
-        uint256 donationId
+        uint256 tokenId,
+        uint256 batchSize
     ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, donationId);
+        require(from == address(this), "Transfer not allowed");
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
+    /**
+     * @dev Overrides supportsInterface to use multiple inheritance in contract
+     */
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(ERC721, ERC721Enumerable) returns (bool) {
