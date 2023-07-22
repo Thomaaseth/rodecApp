58a158227b6bbdbbdb37d5de4fb68a0973354625
Thomaaseth <thomas.demathieu@gmail.com>
2023-07-23 00:40:14 +0200

    deploy sepolia script OK

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers")
 require('hardhat-deploy');
+require('dotenv').config();
+
+const { API_URL, MNEMONIC } = process.env;
 
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -11,6 +14,13 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       chainId: 31337
     },
+    sepolia: {
+      url: API_URL,
+      accounts: {
+        mnemonic: MNEMONIC
+      },
+      chainId: 11155111
+    }
   },
   solidity: "0.8.20",
   namedAccounts: {
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,44 +1,36 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 const fs = require('fs');
+const path = require('path');
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
 
   const donate = await hre.ethers.deployContract("Donate");
-  // const donate = await Donate.deploy();
+  console.log("Donate contract address:", await donate.getAddress());
+
   await donate.waitForDeployment();
 
-  // await donate.deployed();
 
-  // const donateData = {
-  //   address: donate.address,
-  //   abi: JSON.parse(donate.interface.format('json')),
-  // };
+  const donateData = {
+    address: await donate.getAddress(),
+    abi: donate.interface.format('json'),
+  };
 
-  // fs.writeFileSync('donateData.json', JSON.stringify(donateData));
+  const filePath = path.join(__dirname, "../../frontend/contracts/donate.json");
+  fs.writeFileSync(filePath, JSON.stringify(donateData));
+
+  console.log("Contract data written to:", filePath);
 
   console.log(
     `Donate deployed to ${donate.target}`
   );
-
-  // const Marketplace = await hre.ethers.getContractFactory("Marketplace");
-  // const marketplace = await Marketplace.deploy();
-
-  // await marketplace.deployed();
-
-  // console.log(
-  //   `Donate deployed to ${Marketplace.address}`
-  // );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exitCode = (1);
+  });
